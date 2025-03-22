70442dd927cc59f042961673a4c2e7c8c10bfb82
Andre Silva <andrenations@gmail.com>
2025-03-21 23:29:24 -0400

    Added environment variables for app configuration

---
+++ lib/constants/index.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Portfolio';
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION ||'Next Auth 15 Portfolio_25';
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3002';
+
+
