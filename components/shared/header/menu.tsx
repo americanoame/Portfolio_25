8b874845e0d38ae9347325e1c978147ac194653d
Andre Silva <andrenations@gmail.com>
2025-03-22 21:00:07 -0400

    Added responsive menu with sheet component and theme toggle
    - Integrated a menu with a Sheet component for mobile view.
    - Added a button (EllipsisVertical) to trigger the menu on small screens.
    - Included navigation links for Home, About, Projects, and Contact.
    - Added a mode toggle for switching between light and dark themes.
    - Styled menu for mobile responsiveness using TailwindCSS.

---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,57 @@
+import { EllipsisVertical } from "lucide-react";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetTitle,
+  SheetDescription,
+} from "@/components/ui/sheet";
+
+import ModeToggle from "./mode-toggle";
+import Link from "next/link";
+
+const Menu = () => {
+  return (
+    <>
+      <div className="flex justify-end gap-3">
+        
+        <nav className="md:hidden">
+          <Sheet>
+            <SheetTrigger className="align-middle">
+              <EllipsisVertical />
+            </SheetTrigger>
+            <SheetContent className="flex flex-col items-start">
+              <SheetTitle></SheetTitle>
+
+              <nav className="flex flex-col gap-10 p-4 text-[16px]">
+
+              <Link href="/" className="font-light hover:underline">
+                  Home
+                </Link>
+                
+                <Link href="/about" className="font-light hover:underline">
+                  About
+                </Link>
+
+                <Link href="/projects" className="font-light hover:underline">
+                  Projects
+                </Link>
+
+                <Link href="/contact" className="font-light hover:underline">
+                  Contact
+                </Link>
+                
+                <ModeToggle />
+              </nav>
+
+              <SheetDescription></SheetDescription>
+            </SheetContent>
+          </Sheet>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Menu;
